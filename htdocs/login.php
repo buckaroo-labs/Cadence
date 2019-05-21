ce6712d71a201f5cd7f52702f49de4b839785387
Kent Heiner <kent_heiner@byu.net>
2019-05-21 07:45:04 -0700

    fix login logic

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,11 +1,12 @@
 <?php 
 $pagetitle="Log In | Cadence";
 include "Hydrogen/pgTemplate.php";
+$headline = '<h1>Cadence</h1><h3>Keeping in step</h3>' ;
 ?>
 <?php if (isset($_SESSION['demo'])) unset($_SESSION['demo']); ?>
 <!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
 <div class="w3-main" style="margin-left:250px">
-
+<?php include 'Hydrogen/elemLogoHeadline.php';  ?>
   <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
 
